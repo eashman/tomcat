cdd55b99327b53c8a555d46d9b441044717cfd02
Eric Ashman <eashman@drfirst.com>
2014-10-01 14:05:57 -0400

    fix template

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Installs/Configures tomcat'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.16.20'
+version          '0.16.21'
 
 depends 'java'
 depends 'openssl'
